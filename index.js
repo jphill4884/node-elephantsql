16dc7c5c185537a8cd870bb96519c7d95c32a288
Kyle Phillips <justinkphillips@gmail.com>
2022-06-21 12:41:12 +0200

    add route to get order by id

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ app.get("/api/users", (req, res, next) => {
 
 app.get("/api/users/:id", (req, res, next) => {
   const { id } = req.params;
-  console.log(id);
+  //console.log(id);
   pool
-    .query("SELECT * from users WHERE user_id = $1", [id])
+    .query("SELECT * FROM users WHERE user_id = $1", [id])
     .then((data) => res.json(data.rows))
-    .catch(() => res.sendStatus(500));
+    .catch((error) => res.sendStatus(500));
 });
 
 app.post("/api/users", (req, res, next) => {
   const { user_id, first_name, last_name, age, active } = req.body;
   pool
     .query(
-      "INSERT INTO users (user_id, first_name, last_name, age, active) VALUES ($1, $2, $3, $4)", [user_id, first_name, last_name, age, active]
+      "INSERT INTO users (user_id, first_name, last_name, age, active) VALUES ($1, $2, $3, $4, $5)", [user_id, first_name, last_name, age, active]
     )
     .then((data) => res.json(data.rows))
     .catch((error) => res.sendStatus(500));
@@ -39,9 +39,17 @@ app.get("/api/orders", (req, res, next) => {
   pool
     .query("SELECT * FROM orders")
     .then((data) => res.json(data.rows))
-    .catch(() => res.sendStatus(500));
+    .catch((error) => res.sendStatus(500));
 });
 
+app.get("/api/orders/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  pool
+    .query("SELECT * FROM orders WHERE order_id = $1", [id])
+    .then((data) => res.json(data.rows))
+    .catch((error) => res.sendStatus(500));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
